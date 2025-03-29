5a98811e387ffc481d20d629b1f20439bd35fe18
luisfernandovierachagas <luis_fernando_viera_chagas@hotmail.com>
2025-03-29 13:30:11 -0300

    agregue Console

--- Ejercicio2_Lab3/Program.cs
+++ Ejercicio2_Lab3/Program.cs
@@ -7,6 +7,7 @@
             Television television1 = new Television(new Marca("LG", "Corea del Sur"), "QNED 4K", Tamanio.Md, false, "Encendido", 60);
             Television television2 =new Television(new Marca("Sony", "Japon"), "Bravia 8", Tamanio.Xl, true, "Apagado", 100);
 
+            Console.WriteLine("Ejercicio Television");
             Console.WriteLine(television1);
             Console.WriteLine(television2);
             Console.ReadKey();
